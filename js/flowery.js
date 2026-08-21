1cf73093a37cf2b175e7ffa5f07c9af640bb0631
deltarunetomorrow <260947758+deltarunetomorrow@users.noreply.github.com>
2026-08-21 13:24:07 +0200

    DELTARUNE tomorrow...

---
+++ js/flowery.js
@@ -0,0 +1,185 @@
+const audios = [
+    ["All according to- all acording to plant!", "snd_flowery_voiceclip_all_according_to_all_according_to_plant.wav"],
+    ["Blingo Blizzard", "snd_flowery_voiceclip_blingo_blizzard.wav"],
+    ["Calling for help!", "snd_flowery_voiceclip_calling_for_help.wav"],
+    ["Don't you like serving humans?", "snd_flowery_voiceclip_dont_you_like_serving_humans.wav"],
+    ["Flowers blooms in your heart!", "snd_flowery_voiceclip_flowers_blooms_in_your_heart.wav"],
+    ["Flowery!", "snd_flowery_voiceclip_flowery2.wav"],
+    ["Forget it!", "snd_flowery_voiceclip_forget_it.wav"],
+    ["Get a chance! (1)", "snd_flowery_voiceclip_get_a_chance_1.wav"],
+    ["Get a chance! (2)", "snd_flowery_voiceclip_get_a_chance_2.wav"],
+    ["Give to you!", "snd_flowery_voiceclip_give_to_you.wav"],
+    ["Glue!", "snd_flowery_voiceclip_glue.wav"],
+    ["Go home", "snd_flowery_voiceclip_go_home.wav"],
+    ["Goodbye", "snd_flowery_voiceclip_goodbye.wav"],
+    ["Great style", "snd_flowery_voiceclip_great_style.wav"],
+    ["Grown like a turnip!", "snd_flowery_voiceclip_grown_like_a_turnip.wav"],
+    ["Hah!", "snd_flowery_voiceclip_hah.wav"],
+    ["Heh, it's my Jarona!", "snd_flowery_voiceclip_heh_it_s_my_jarona.wav"],
+    ["Here I come-!", "snd_flowery_voiceclip_hereicome.wav"],
+    ["Here I come San Frandisco! (Strong)", "snd_flowery_voiceclip_hereicomesanfrandisco_strong.wav"],
+    ["Here I come San Frandisco! (Weak)", "snd_flowery_voiceclip_hereicomesanfrandisco_weak.wav"],
+    ["Hey", "snd_flowery_voiceclip_hey.wav"],
+    ["Hey Raly", "snd_flowery_voiceclip_hey_raly.wav"],
+    ["Hey guys", "snd_flowery_voiceclip_heyguys.wav"],
+    ["Hey guys, I think I found a glue!", "snd_flowery_voiceclip_heyguysithinkifoundaglue.wav"],
+    ["Hey there little guy!", "snd_flowery_voiceclip_heytherelittleguy.wav"],
+    ["Hoo!", "snd_flowery_voiceclip_hoo.wav"],
+    ["Huh!", "snd_flowery_voiceclip_huh.wav"],
+    ["Huh, I'll show you!", "snd_flowery_voiceclip_huhillshowyou.wav"],
+    ["I'm falling!", "snd_flowery_voiceclip_im_falling.wav"],
+    ["I'm only trying to help you!", "snd_flowery_voiceclip_im_only_trying_to_help_you.wav"],
+    ["I'm sorry once again I kept a lady in waiting!", "snd_flowery_voiceclip_imsorryonceagainikeptaladyinwaiting.wav"],
+    ["It's all in a name!", "snd_flowery_voiceclip_its_all_in_a_name.wav"],
+    ["It's all yours!", "snd_flowery_voiceclip_its_all_yours.wav"],
+    ["It's so human!", "snd_flowery_voiceclip_its_so_human.wav"],
+    ["It's me!", "snd_flowery_voiceclip_itsme.wav"],
+    ["It's me, Flowery!", "snd_flowery_voiceclip_itsmeflowery.wav"],
+    ["Jarona! (1)", "snd_flowery_voiceclip_jarona1.wav"],
+    ["Jarona! (2)", "snd_flowery_voiceclip_jarona2.wav"],
+    ["Jarona! (3)", "snd_flowery_voiceclip_jarona3.wav"],
+    ["Jarona! (4)", "snd_flowery_voiceclip_jarona4.wav"],
+    ["Last Jarona!", "snd_flowery_voiceclip_last_jarona.wav"],
+    ["Leaf it to me!", "snd_flowery_voiceclip_leaf_it_to_me.wav"],
+    ["Lend me your power!", "snd_flowery_voiceclip_lend_me_your_power.wav"],
+    ["Minipeppers!", "snd_flowery_voiceclip_minipeppers.wav"],
+    ["Mostlys!", "snd_flowery_voiceclip_mostlys.wav"],
+    ["My favourite two!", "snd_flowery_voiceclip_my_favorite_two.wav"],
+    ["My human!", "snd_flowery_voiceclip_my_human.wav"],
+    ["My king!", "snd_flowery_voiceclip_my_king.wav"],
+    ["Mysterious wind!", "snd_flowery_voiceclip_mysterious_wind.wav"],
+    ["No way it's your children!", "snd_flowery_voiceclip_no_way_its_your_children.wav"],
+    ["No, no, no!", "snd_flowery_voiceclip_nonono.wav"],
+    ["Omega Flowery!", "snd_flowery_voiceclip_omega_flowery.wav"],
+    ["Prism Blow!", "snd_flowery_voiceclip_prism_blow.wav"],
+    ["Say that again!", "snd_flowery_voiceclip_say_that_again.wav"],
+    ["Smile again!", "snd_flowery_voiceclip_smile_again.wav"],
+    ["Sorry about that guys!", "snd_flowery_voiceclip_sorryaboutthatguys.wav"],
+    ["Sorry about that little guy!", "snd_flowery_voiceclip_sorryaboutthatlittleguy.wav"],
+    ["Sorry about the guy!", "snd_flowery_voiceclip_sorryabouttheguy.wav"],
+    ["Sorry to keep a lady in waiting!", "snd_flowery_voiceclip_sorrytokeepaladyinwaiting.wav"],
+    ["Sorry to keep you ladies!", "snd_flowery_voiceclip_sorrytokeepyouladies.wav"],
+    ["Sorry to keep you waiting! (1)", "snd_flowery_voiceclip_sorrytokeepyouwaiting1.wav"],
+    ["Sorry to keep you waiting! (2)", "snd_flowery_voiceclip_sorrytokeepyouwaiting2.wav"],
+    ["Sustingus!", "snd_flowery_voiceclip_stingus.wav"],
+    ["Suckle it up!", "snd_flowery_voiceclip_suckle_it_up.wav"],
+    ["Susie!", "snd_flowery_voiceclip_susie.wav"],
+    ["Take that!", "snd_flowery_voiceclip_take_that.wav"],
+    ["That's my dreams!", "snd_flowery_voiceclip_thats_my_dreams.wav"],
+    ["That's great!", "snd_flowery_voiceclip_thatsgreat.wav"],
+    ["The boys!", "snd_flowery_voiceclip_the_boys.wav"],
+    ["The diner!", "snd_flowery_voiceclip_the_diner.wav"],
+    ["They're eating my flesh!", "snd_flowery_voiceclip_theyre_eating_my_flesh.wav"],
+    ["This guy's your best friend!", "snd_flowery_voiceclip_thisguysyourbestfriend.wav"],
+    ["Try my flavor!", "snd_flowery_voiceclip_try_my_flavor.wav"],
+    ["What a predictable creature!", "snd_flowery_voiceclip_what_a_predictable_creature.wav"],
+    ["With your powers combined!", "snd_flowery_voiceclip_with_your_powers_combined.wav"],
+    ["Wow!", "snd_flowery_voiceclip_wow.wav"],
+    ["Yes!", "snd_flowery_voiceclip_yes.wav"],
+    ["Your dad!", "snd_flowery_voiceclip_your_dad.wav"],
+    ["Your dad's my best friend!", "snd_flowery_voiceclip_yourdadsmybestfriend.wav"],
+    ["You're a hero?", "snd_flowery_voiceclip_youre_a_hero.wav"],
+    ["Kris", "snd_flowery_voiceclip_kris.wav"]
+];
+
+const textInterline = 2;
+const charWidth = 8;
+const charHeight = 16;
+
+let boardSize;
+let boardTop;
+let boardLeft;
+
+let vWidth = null;
+let vHeight = null;
+
+function drawText(num) {
+    const textColor = "#fee502"
+    let mystring = audios[num][0];
+    let y = boardTop - charHeight*2 - textInterline*2;
+    let x = boardLeft + Math.floor((boardSize - (mystring.length * charWidth))/2);
+    let mystringSliced = null;
+    let prevChar = null;
+    let prevString = null;
+    let prevX = null;
+    let xx = [];
+    let mystrings = [];
+    if (x < boardLeft) {
+        let curChar = 0;
+        while(mystring.indexOf(" ", curChar) !== -1) {
+            prevChar = curChar;
+            prevString = mystring.substring(0,prevChar);
+            curChar = mystring.indexOf(" ", curChar)+1;
+            mystringSliced = mystring.substring(0,curChar);
+            prevX = x;
+            x = boardLeft + Math.floor((boardSize-(mystringSliced.length*charWidth)) / 2);
+            if (x < boardLeft) {
+                xx.push(prevX + charWidth/2);
+                mystrings.push(prevString);
+                mystring = mystring.slice(prevChar);
+            }
+        }
+        x = boardLeft + Math.floor((boardSize-(mystring.length*charWidth)) / 2);
+    }
+    xx.push(x);
+    mystrings.push(mystring);
+    let info = "";
+    for (let j=0; j<xx.length; j++) {
+        info += `<span class="msg${num}" style="position:absolute;color:${textColor};font-size:${charHeight}px;left:${xx[j]}px;top:${y}px;">${mystrings[j]}</span>`;
+        y += textInterline + charHeight;
+    }
+    document.getElementById("message").innerHTML = info;
+}
+
+function playSound(num) {
+    let tile = document.getElementById(`tile${num}`);
+    let prevcol = num%2==0?"#fee502":"#fff06f";
+    let backcol = "#716a3c";
+    tile.style.backgroundColor = backcol;
+    const audio = new Audio();
+    audio.src = "snd/" + audios[num][1];
+    audio.play();
+    audio.onpause = function() {
+        tile.style.backgroundColor = prevcol;
+        document.querySelectorAll(`.msg${num}`).forEach((e) => e.remove());
+    }
+    drawText(num);
+}
+
+function drawBoard() {
+    if (window.visualViewport.width === vWidth && window.visualViewport.height === vHeight) {
+        return null;
+    }
+    vWidth = window.visualViewport.width;
+    vHeight = window.visualViewport.height;
+    const maxWidth = vWidth - 4;
+    const maxHeight = vHeight - charHeight*2 - textInterline*3 - 4;
+    boardSize = Math.min(maxWidth, maxHeight);
+    boardTop = Math.floor((vHeight - (boardSize - charHeight*2 - textInterline*3))/2);
+    boardLeft = Math.floor((vWidth - boardSize)/2);
+    const soundboard = document.getElementById("soundboard");
+    const tileSize = boardSize/9;   // 9 tiles per row and column
+    let tileTop = boardTop;
+    let tileLeft = boardLeft;
+    let html = "";
+    for (let i = 0; i < audios.length; i++) {
+        let backcol = i%2==0?"#fee502":"#fff06f";
+        let textWidth = `${i+1}`.length * charWidth;
+        let textTop = tileTop + Math.floor((tileSize - charHeight)/2);
+        let textLeft = tileLeft + Math.floor((tileSize - textWidth)/2);
+        let tile = `<div id="tile${i}" onclick="playSound(${i})" style="position:absolute;top:${tileTop}px;left:${tileLeft}px;width:${tileSize}px;height:${tileSize}px;background-color:${backcol};text-align:center;"></div><span onclick="playSound(${i})" style="position:absolute;user-select:none;top:${textTop}px;left:${textLeft}px;font-size:16px;color:black;">${i+1}</span>`;
+        html += tile;
+        tileLeft += tileSize;
+        if ((i+1)%9===0) {
+            tileTop += tileSize;
+            tileLeft = boardLeft;
+        }
+    }
+    soundboard.innerHTML = html;
+}
+
+function main() {
+    let interval = setInterval(drawBoard, 1000/30);
+}
+
+document.addEventListener("DOMContentLoaded", main);
